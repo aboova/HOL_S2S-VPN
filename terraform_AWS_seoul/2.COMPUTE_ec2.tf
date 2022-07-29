16c9a205b90f8786b814a1f1cddc26cfd526c824
wowjd <wowjd@megazone.com>
2022-07-29 12:55:55 +0900

    ami change

--- terraform_AWS_seoul/2.COMPUTE_ec2.tf
+++ terraform_AWS_seoul/2.COMPUTE_ec2.tf
@@ -1,6 +1,6 @@
 ##Public Instance
 resource "aws_instance" "bastion" {
-  ami           = "ami-0b7546e839d7ace12" #리전 별 AMI가 다르니 참고
+  ami           = "ami-0fd0765afb77bcca7" #리전 별 AMI가 다르니 참고
   instance_type = "t3.micro"
   vpc_security_group_ids = [
     aws_security_group.public-sg.id,
@@ -26,7 +26,7 @@ resource "aws_instance" "bastion" {
 
 ##Private Instance
 resource "aws_instance" "private_ec2" {
-  ami           = "ami-0b7546e839d7ace12"
+  ami           = "ami-0fd0765afb77bcca7"
   instance_type = "t3.micro"
   vpc_security_group_ids = [
     aws_security_group.private-sg.id,
